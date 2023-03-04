150cd35aa3c655746a7d46784c270aa72f42b4bc
dmasuda1116 <dmasuda1116@gmail.com>
2023-03-04 14:12:12 +0900

    no message

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,23 @@ import "./App.css";
 function App() {
   const [message, setMessage] = useState("");
   const [error, setError] = useState("");
-
+// sendMessages call
+  if (!liff.isInClient()) {
+    window.alert('This button is unavailable as LIFF is currently being opened in an external browser.');
+  } else {
+    liff.sendMessages([
+        {
+            type: 'text',
+            text: 'Hello, World!',
+        },
+    ])
+        .then(() => {
+            window.alert('Message sent');
+        })
+        .catch((error) => {
+            window.alert('Error sending message: ' + error);
+        });
+  }
   useEffect(() => {
     liff
       .init({
